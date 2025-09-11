34e92fd6351a66b47f74e348c2a22725b817791b
dmilkie <dmilkie@gmail.com>
2025-09-11 15:05:02 -0400

    Create launch.bat
    Example of launching and detatching six workers

---
+++ examples/build-your-own-image/launch.bat
@@ -0,0 +1,6 @@
+docker run -d --hostname ubuntu-compiler1 --rm fpgacompile:latest
+docker run -d --hostname ubuntu-compiler2 --rm fpgacompile:latest
+docker run -d --hostname ubuntu-compiler3 --rm fpgacompile:latest
+docker run -d --hostname ubuntu-compiler4 --rm fpgacompile:latest
+docker run -d --hostname ubuntu-compiler5 --rm fpgacompile:latest
+docker run -d --hostname ubuntu-compiler6 --rm fpgacompile:latest
